3b4546ac027b836783d09663216a9d358263849b
madhav-agarwal353 <madhavagarwal042@gmail.com>
2026-02-08 15:37:47 +0530

    axios file chnages

--- frontend/src/configs/axios.ts
+++ frontend/src/configs/axios.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
-
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000',
+    baseURL: import.meta.env.VITE_BASEURL || 'http://localhost:3000',
     withCredentials: true,
 }) 
